4b185b9710e010c11c6e2a096378c5eae544462e
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2021-11-28 16:22:25 -0600

    Added contact field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,29 +3,30 @@ import { Form, Button, Card } from 'react-bootstrap'
 
 function Contact() {
   return(
-    <Card id='contact'>
+    <Card id='contact' className = 'row col-10 m-auto'>
       <Card.Title>Contact Me</Card.Title>
 
 
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" required/>
           <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="string" placeholder="Your name" required />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Tell me something</Form.Label>
+          <Form.Control type="string" placeholder="Your message" required />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
+
       </Form>
     </Card>
   )
